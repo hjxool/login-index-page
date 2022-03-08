fa4b128ab65a32560ee9e2337fad58ef9d920ee8
hjxool <691638334@qq.com>
2022-03-08 11:09:22 +0800

    抽出滑块和电平显示组件

---
+++ temp.js
@@ -0,0 +1,29 @@
+new Vue({
+	el: '#temp',
+	data() {
+		return {
+			sound_item_options: [
+				{ name: '功放参数', light_icon: './img/功放icon亮.png', dark_icon: './img/功放icon暗.png' },
+				{ name: 'DSP参数', light_icon: './img/dsp参数亮.png', dark_icon: './img/dsp参数暗.png' },
+				{ name: '系统功能', light_icon: './img/系统功能亮.png', dark_icon: './img/系统功能暗.png' },
+			],
+			option_focus: 0,
+		};
+	},
+	methods: {
+		option_style(index) {
+			let style = {
+				background: this.option_focus == index ? '#1431a0' : 'rgba(32, 44, 92, 0.48)',
+				border: this.option_focus == index ? '' : '1px solid #1B59CA',
+				boxShadow: this.option_focus == index ? '' : '0px 0px 65px 0px rgba(4, 15, 51, 0.8)',
+			};
+			return style;
+		},
+		// aaa() {
+		// 	let a = $('.amplify_box').eq(0);
+		// 	console.log(a);
+		// 	let b = $('.amplify_box')[0];
+		// 	console.log(b);
+		// },
+	},
+});
